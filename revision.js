8e8ff2fa33ab1b149934ba646ddfc9d36f5c7ca6
unknown <Minskoleopc>
2023-11-01 07:57:36 -0700

    added

---
+++ revision.js
@@ -0,0 +1,48 @@
+let a = 10 
+console.log(a)
+a = 100
+console.log(a)
+
+
+const h = 100 
+console.log(h)
+//h = 100
+
+
+// Arithmetic 
+
+// addition // subtraction // multiplication // division // modules
+
+let q = 8 
+let r = 4
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+let a1 = 9 
+let a2 = 3
+
+console.log(a1+a2)
+console.log(a1-a2)
+console.log(a1*a2)
+console.log(a1/a2)
+console.log(a1%a2)
+
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+
+Calculator(8,4)
+Calculator(9,3)
+Calculator(6,3)
+
+
+// 2 pm revision 
