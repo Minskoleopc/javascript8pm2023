9ba6de54b3f1da9f71d0894dd3a5004a58542d06
unknown <Minskoleopc>
2023-11-04 07:56:04 -0700

    added

---
+++ script6.js
@@ -0,0 +1,64 @@
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// print 1 to 100  for loop and while 
+// for(intialization ; conditionCheck , increment/decrement){
+//     // statements
+// }       
+// print 1 to 3
+for(let i = 1 ; i <= 3 ; i++){ // 2 // 3 //4
+    console.log(i) // 1 // 2 // 3
+}
+
+// 1 <= 3 ===> true
+// 2 <= 3 ===> true
+// 3 <= 3 ===> true
+
+// program 2
+// print hello 3 types
+for(let i = 1 ; i <= 3 ; i++){ // 2 // 3 // 4
+    console.log("hello")  // hello // hello // hello
+
+}
+
+// program 3
+// print 1 to 5
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 //3 // 4 // 5
+}
+
+// program 4
+// print(5 to 1)
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 
+    console.log(i) //5 // 4 // 3 // 2 //1
+}
+
+// print 10 to 1 
+// program 5
+for(let i = 10 ; i >= 1 ; i--){
+    console.log(i)
+}
+
+// table of two
+//program 6
+
+for(let i = 1 ; i <= 10 ; i++){ // 2 ------------- 11
+    console.log(i * 2) // 2 // 4 ---------------20
+}
+
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 // 6 ------ 22
+    console.log(i) // 2 // 4 ------ 20
+}
+
+
+
+
+
+
+
+
+
+
