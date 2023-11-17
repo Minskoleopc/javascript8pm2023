c7f2ac884f08931ce74ec3ffc92a0e6da44e55f9
unknown <Minskoleopc>
2023-11-17 06:58:13 -0800

    added

---
+++ revisionDay1.js
@@ -0,0 +1,132 @@
+// variables 
+// program 1
+let a = 10 
+console.log(a)
+a = 100 
+console.log(a)
+
+
+// program 2
+const h = 100 
+console.log(h)
+//h = 900
+
+// Arithetic 
+//   +  , - , * , / , %  
+
+let x1 = 10
+let x2 = 5
+
+console.log(x1+x2)
+console.log(x1-x2)
+console.log(x1*x2)
+console.log(x1/x2)
+console.log(x1%x2) 
+
+let s = 8 
+let t = 4
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+// functions
+
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x*y)
+}
+Calculator(12,4)
+Calculator(8,4)
+Calculator(9,3)
+Calculator(6,3)
+
+// function without parameter and without return type 
+
+function addition(){
+    console.log(8+8)
+}
+addition()
+addition()
+addition()
+addition()
+
+
+
+// function with parameter and without return type 
+
+function additionB(x,y){
+    console.log(x+y)
+}
+additionB(12,4) // 16
+additionB(12,3) // 15
+additionB(12,5) // 17
+additionB(12,2) // 14
+
+// function with parameter and with return type 
+function additionC(x,y){
+    return x + y
+}
+let q1 = additionC(12,4)
+console.log(q1) // 16
+console.log(q1 + 4) // 20
+console.log(q1 - 4) // 12
+
+// Extrovert // Introvert 
+// social       // less social
+// loud         // calm
+// more outing  // less introvert
+
+// Human 
+// Property --- weight , height , age , color
+// Method - talk() , walk()
+
+//Vehicle 
+//Property - color , regNo , type 
+//Method - start() , stop()
+
+// Bank acc - 
+// Property -  accNo , accType , bal
+// Method - Deposit() , WithDrawl()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
