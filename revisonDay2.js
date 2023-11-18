fe044e19b507f7df4657a02b185600edd4f81937
unknown <Minskoleopc>
2023-11-18 06:57:37 -0800

    added

---
+++ revisonDay2.js
@@ -0,0 +1,109 @@
+
+// program 1
+let x = 10 
+console.log(x)
+console.log(typeof x)
+// 10 , -10 ,0 ,77.8 , -99.99
+
+// program 2
+
+let y = true 
+console.log(y)
+console.log(typeof y)
+// true or false 
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z) // string
+// "A" , "a" , "123", " ","#", "3432#@$#@"
+
+
+
+// comparison operator 
+
+// < , >  , <= , >=  
+// == , !=   // value
+// === , !==  // value and type
+
+console.log(typeof '10') // string
+console.log(typeof 10) // number
+
+// entity < entity   // true or false
+
+console.log(7 == 7)
+console.log(7 == '7')
+console.log(7 === 7)
+console.log(7 === '7')
+console.log(7 !== '7')
+console.log(7 !== 7)
+console.log(7 < 3)
+console.log(7 > 3)
+console.log(7 >= 3)
+console.log(7 <= 3)
+console.log(7 >= 7)
+console.log(7 <= 7)
+console.log(7 != '7')
+console.log(7 != 7)
+
+
+// Logical operator
+// && - AND 
+
+// true     &&     true    ======> true 
+// false    &&     true    =======> false 
+// true     &&     false   =======> false 
+// false    &&     false   =======> false 
+
+console.log(7 == 7 && 8 == 8)
+console.log(7 != 7 && 8 == 8)
+console.log(7 == 7 && 8 != 8)
+console.log(7 != 7 && 8 != 8)
+
+
+// Or -- 
+
+// true  ||   true     =======> true 
+// false ||   true     =======> true 
+// true  ||   false    =======> true 
+// false ||   false    =======> false
+
+
+console.log(7 == 7 || 8 == 8) // true
+console.log(7 != 7 || 8 == 8) // true
+console.log(7 == 7 || 8 != 8) // true 
+console.log(7 != 7 || 8 != 8) // false
+
+// Not
+// True     ====> !    False 
+// False    ====> !    True
+
+console.log(! (2 == 2))
+console.log(!(2 != 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
