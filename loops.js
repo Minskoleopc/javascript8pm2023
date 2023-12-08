9b91418bd0e78d75976462db3c6810aee8bd310d
unknown <Minskoleopc>
2023-12-08 08:30:20 +0530

    added

---
+++ loops.js
@@ -0,0 +1,73 @@
+// loops  for and while
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// for loop
+// for(intialization; conditionCheck ; increment/decrement){
+//     // statement
+// }
+
+// program 2
+for (let i = 1; i <= 3; i++) {  // 2 // 3 // 4
+
+    console.log(i) // 1 // 2 // 3
+
+}
+
+
+// program 2
+// 1 <= 3  ---- true
+// 2 <= 3  ----- true
+// 3 <= 3  ------ true
+
+for (let i = 1; i <= 3; i++) { // 2 // 3 // 4
+    console.log("hello") // hello // hello // hello
+}
+
+// program 3
+
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 // 3 // 4 // 5
+}
+
+// program 4
+
+for(let i = 5 ; i >= 1 ; i --){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3  // 2 // 1
+} 
+
+// program 5 
+for(let i = 3 ; i >= 1 ; i--){ // 2 // 1 //0
+    console.log(i) // 3 // 2 // 1
+}
+
+
+// program 6 
+// table of 2
+
+for(let i = 1 ; i <= 10 ; i++){ // 2 // 3 ------- 11
+    console.log(i * 2) // 2 // 4 // 6 ------ 20
+}
+
+// table of 5 in reverse
+// program 7
+for(let i = 10 ; i >= 1 ; i--){ // 9 ---------------0
+    console.log(i * 5) // 50 // 45 ------- 5
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
