c905ab4a800b9c03b970927aeae622ab78553052
unknown <Minskoleopc>
2023-12-11 20:29:36 +0530

    added

---
+++ script13.js
@@ -0,0 +1,20 @@
+// program 1
+
+//              0         1          2    3
+let names = ["chinmay","shirish","ninad","ram"]
+//             -4        -3         -2     -1
+//names.slice(startIndex, endIndex(not including))
+
+// let a = names.slice(0)
+// console.log(a)
+// console.log(names.slice(1,3))
+// console.log(names.slice(0,-1))
+// console.log(names.slice(-1,0))
+
+
+// names = ["chinmay","shirish","ninad","ram"]
+// // names.pop()
+// // names.shift()
+// // names.splice(startIndex,numberOfElementTobeDeleted)
+// names.splice(1,2,"shivani","kajal")
+// console.log(names)
