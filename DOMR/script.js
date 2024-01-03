0e4e8b62975c2e6f130d524a7263eac2cbc6d3fb
unknown <Minskoleopc>
2024-01-03 20:28:14 +0530

    added

---
+++ DOMR/script.js
@@ -0,0 +1,31 @@
+
+//<h1 id = "one" class = "two">Chinmay</h1>
+// Program 1
+// let byTagName = document.querySelector('h1')
+// console.log(byTagName)
+
+// let byId  = document.querySelector('#one')
+// console.log(byId)
+
+// let byClass  = document.querySelector('.two')
+// console.log(byClass)
+
+// let byHeading = document.querySelector('h1[id="one"]')
+// console.log(byHeading)
+
+
+// byHeading.addEventListener('click',function(){
+//     byHeading.style.color = "red"
+// })
+
+// program 2
+
+{/* <h1 id = "one" class = "two">Chinmay</h1>
+<button>ChangeColor</button> */}
+
+// let byHeadElement = document.querySelector('#one')
+// let changeColorButton = document.querySelector('button')
+
+// changeColorButton.addEventListener('click',function(){
+//     byHeadElement.style.color = "red"
+// })
