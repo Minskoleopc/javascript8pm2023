97a2e8f16c19c1885b8df0d4821b39ce29a5e82d
unknown <Minskoleopc>
2024-02-09 20:35:00 +0530

    added

--- Oops/day4.js
+++ Oops/day4.js
@@ -69,18 +69,98 @@ let q6 = addCity(amolB)
 console.log(q6)
 
 // set as parameter and set as return 
-
 // map as parameter and map as a return 
 
 // function as a paramter
-
 // function as a return type
+// function declaration
 
 
+// function additionC(x,y){
+//     return x + y
+// }
+
+// let a = additionC(12,3)
+// console.log(a)
 
 
+// let  additionD = function(x,y){
+//     return x + y
+// }
+// let b = addition(23,4)
+// console.log(b)
 
 
+// arrow function
+let additionE = (x,y)=>{
+    return x + y
+}
+let q11 = additionE(12,4)
+console.log(q11)
+
+// program 1
+let x = 10 
+console.log(x)
+x = 200
+console.log(x)
+
+let add = function(x,y){
+    console.log(x+y)
+}
+add(12,4) // call 
+console.log(add)
+
+// program 2
+add = function(x,y){
+    return x + y
+}
+function additionA(fn,x,y){
+
+    //let x = 10
+    // let y = 20
+    // let fn = function(x,y){
+    //     return x + y
+    // }
+    let q1 = fn(x,y)
+    return q1
+}
+let q22 = additionA(add,10,20)
+console.log(q22)
+
+
+let sub = function(x,y){
+    return x - y
+}
+
+//console.log(sub)
+function subtraction(fn,a,b){
+    // let fn = function(x,y){
+    //     return x - y
+    // }
+    //let a = 12
+    // let b = 3 
+
+
+    let q2 = fn(a,b)
+    return q2
+}
+let q32 = subtraction(sub,12,3)
+console.log(q32)
+
+// program 4 -------------------
+// function ads a return type
+function greet(){
+    return function(){
+        console.log("hello")
+    }
+}
+let a = greet()
+console.log(a)
+
+// let a = function(){
+//     console.log("hello")
+// }
+a()
 
 
 
